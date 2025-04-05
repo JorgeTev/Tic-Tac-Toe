b624ca4bc71d963a59f752f2a707d95a8e17f28c
Jorge Luis Quispe Teves <jorgeluistevespro@gmail.com>
2025-04-05 18:56:42 -0500

    star new project

--- projects/Tic-Tac-Toe/src/App.jsx
+++ projects/Tic-Tac-Toe/src/App.jsx
@@ -1,34 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const TURN = {
+  X:'x',
+  O:'o'
+}
+
+
+const Square = ({children, isSelected, updateBoard, index}) => {
+  const className= `square ${isSelected ? 'is-selected': ''}`
+
+  const handleClick = () =>{
+    updateBoard()
+  }
+  return (
+    <div className="square">
+      {children}
+    </div>
+  )
+}
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [board, setBoard]= useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURN.X)
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) =>{
+    for (const combo of WINNER_COMBOS){
+      const [a, b, c] = combo
+      if(
+        newBoard[a] &&
+        newBoard[a] === newBoard[b] &&
+        newBoard[a] === newBoard[c]
+      ){return boardToCheck[a]}
+    }
+    return null
+  }
+
+  const updateBoard = (index) => {
+    if(board[index] || winner) return
+
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURN.X ? TURN.O : TURN.X
+    setTurn(newTurn)
+
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      setWinner((prevWinner)=>{
+        console.log(`Ganador: ${newWinner}, el anterior era ${prevWinner}`)
+        return newWinner
+      })
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className='board'>
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {
+          board.map((turn, index) => {
+            return (
+              <Square
+                key = {index}
+                index = {index}
+                updateBoard={updateBoard}
+              >
+                {index}
+              </Square>
+            )
+          })
+        }
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURN.X}> {TURN.X} </Square>
+        <Square isSelected={turn === TURN.O}> {TURN.O} </Square>
+      </section>
+    </main>
   )
 }
 
